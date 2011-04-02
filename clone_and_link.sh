b9824539c6aff679d0228df7bc31b31be36c4a5b
Shanon McQuay <shanonmcquay@gmail.com>
2011-04-02 16:58:56 +1100

    Add a script to support installing dot-files from babushka

---
+++ clone_and_link.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd &&
+[ -d '.dot-files' ] || git clone git://github.com/compactcode/dot-files.git .dot-files &&
+ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
+  [ "$f" == '.dot-files/files/.' ] ||
+  [ "$f" == '.dot-files/files/..' ] ||
+  [ "$f" == '.dot-files/files/.git' ] ||
+  ln -vsf "$f" .
+done
