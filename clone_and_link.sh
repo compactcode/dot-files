d5f2d49bcdecc8c9c6bb5ef37299e4e84699bb79
Shanon McQuay <shanonmcquay@gmail.com>
2011-09-06 00:14:17 +1000

    Clone in oh-my-zsh at the same time as dot files.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd &&
-[ -d '.dot-files' ] || git clone git://github.com/compactcode/dot-files.git .dot-files &&
+([ -d '.dot-files' ] || git clone git://github.com/compactcode/dot-files.git .dot-files) &&
+([ -d '.oh-my-zsh' ] || git clone git://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh) &&
 ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
   [ "$f" == '.dot-files/files/.' ] ||
   [ "$f" == '.dot-files/files/..' ] ||
