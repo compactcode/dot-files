b01b9ad0cd77e07974b0f61bede3515f8fd03cc8
Shanon McQuay <shanonmcquay@gmail.com>
2011-09-25 20:34:04 +1000

    Load oh-my-zsh using a submodule.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -2,7 +2,6 @@
 
 cd &&
 ([ -d '.dot-files' ] || git clone git://github.com/compactcode/dot-files.git .dot-files) &&
-([ -d '.oh-my-zsh' ] || git clone git://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh) &&
 ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
   [ "$f" == '.dot-files/files/.' ] ||
   [ "$f" == '.dot-files/files/..' ] ||
