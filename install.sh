4641b0f020a6e0913164d81c409a1e42e860e0b6
Shanon McQuay <shanonmcquay@gmail.com>
2012-03-04 21:22:24 +1100

    Enable 8 bit support in terminal.

--- install.sh
+++ install.sh
@@ -10,9 +10,10 @@ ln -vsf "$MY_PATH/files/.aliases" ~
 ln -vsf "$MY_PATH/files/.bash_profile" ~
 ln -vsf "$MY_PATH/files/.bashrc" ~
 ln -vsf "$MY_PATH/files/.gemrc" ~
-ln -vsf "$MY_PATH/files/.gitignore" ~
 ln -vsf "$MY_PATH/files/.gitconfig" ~
+ln -vsf "$MY_PATH/files/.gitignore" ~
 ln -vsf "$MY_PATH/files/.hushlogin" ~
+ln -vsf "$MY_PATH/files/.inputrc" ~
 ln -vsf "$MY_PATH/files/.oh-my-zsh" ~
 ln -vsf "$MY_PATH/files/.rbenv" ~
 ln -vsf "$MY_PATH/files/.rspec" ~
