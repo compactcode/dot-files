a7fd1f5205971e9d0dc4d8c449afcd48f1671f9f
Shanon McQuay <shanonmcquay@gmail.com>
2016-07-15 11:47:48 +1000

    Remove some old guff.

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ git submodule update --init
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
 
-ln -vsf "$MY_PATH/files/.ackrc" ~
 ln -vsf "$MY_PATH/files/.aliases" ~
 ln -vsf "$MY_PATH/files/.bash_profile" ~
 ln -vsf "$MY_PATH/files/.bashrc" ~
@@ -15,7 +14,6 @@ ln -vsf "$MY_PATH/files/.gitignore" ~
 ln -vsf "$MY_PATH/files/.hushlogin" ~
 ln -vsf "$MY_PATH/files/.inputrc" ~
 ln -vsf "$MY_PATH/files/.oh-my-zsh" ~
-ln -vsf "$MY_PATH/files/.rbenv" ~
 ln -vsf "$MY_PATH/files/.rspec" ~
 ln -vsf "$MY_PATH/files/.vim" ~
 ln -vsf "$MY_PATH/files/.vimrc" ~
