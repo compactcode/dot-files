3cd6972bc3b2d97ea712df3546efeef727693e7e
Shanon McQuay <shanonmcquay@gmail.com>
2017-05-31 14:00:00 +1000

    Add an fzf function to cd to a sub directory.

--- files/.functions/fzf-git.sh
+++ files/.functions/fzf-git.sh
@@ -1,3 +1,10 @@
+# fd - cd to selected directory
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -type d -print | grep -v '\.git' 2> /dev/null | fzf +m) && cd "$dir"
+}
+
+# fco - checkout a git branch
 fco() {
   local branches branch
   branches=$(git for-each-ref --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
