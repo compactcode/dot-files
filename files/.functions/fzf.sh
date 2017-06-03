eea64a9e234359c033170b43b23d51642c8a4e08
Shanon McQuay <shanonmcquay@gmail.com>
2017-06-03 18:48:55 +1000

    Show remote branches when checking out.

--- files/.functions/fzf.sh
+++ files/.functions/fzf.sh
@@ -7,8 +7,8 @@ fd() {
 # fco - checkout a git branch
 fco() {
   local branches branch
-  branches=$(git for-each-ref --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branches=$(git branch --all) &&
   branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+           fzf -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
