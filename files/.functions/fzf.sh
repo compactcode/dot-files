3e9665b1d674b3f7a4e876e129bc0f05b3517b99
Shanon McQuay <shanonmcquay@gmail.com>
2017-06-03 19:05:27 +1000

    Start on the fuzzy search function.

--- files/.functions/fzf.sh
+++ files/.functions/fzf.sh
@@ -12,3 +12,11 @@ fco() {
            fzf -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
+
+# frg - search files and select a result
+frg() {
+  # TODO: Require param.
+  # TODO: Handle no results.
+  # TODO: Cleanup the selected result.
+  rg --column --no-heading --hidden -i --color always $@ | grep -v '\.git' | fzf --ansi --preview '~/.functions/preview-search-result.sh {}'
+}
