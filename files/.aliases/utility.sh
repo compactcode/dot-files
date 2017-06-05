e1545781ebf48979f66a86ec069a090ff3f42f38
Shanon McQuay <shanonmcquay@gmail.com>
2017-06-05 19:03:15 +1000

    Add an alias to combine fasd with fzf.

--- files/.aliases/utility.sh
+++ files/.aliases/utility.sh
@@ -1,5 +1,3 @@
 alias ls='ls -G'
 alias l='ls -la'
 alias md='mkdir -p'
-
-alias jj='cd $(fasd_cd -l | tail -1)'
--- files/.functions/fzf.sh
+++ files/.functions/fzf.sh
@@ -20,3 +20,31 @@ frg() {
   # TODO: Cleanup the selected result.
   rg --column --no-heading --hidden -i --color always $@ | grep -v '\.git' | fzf --ansi --preview '~/.functions/preview-search-result.sh {}'
 }
+
+# fj - cd to selected mru directory
+#
+# fj                => select => cd
+# fj search_pattern => cd
+fj() {
+  local search_pattern selected_directory
+
+  search_pattern=$1
+
+  if [[ -n $search_pattern ]]
+  then
+    selected_directory=$(fasd -dlR $search_pattern | head -1)
+  else
+    selected_directory=$(fasd -dlR | fzf)
+  fi
+
+  if [[ -n $selected_directory ]]
+  then
+    cd $selected_directory
+  fi
+}
+
+
+# Go to a frequently used directory.
+alias j="fj"
+# Go straight to your most used directory.
+alias jj="fasd -dlR | head -1"
