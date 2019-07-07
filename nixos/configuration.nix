55e018d6eebbdc70908246ab69a11bc38ea4e3d6
Shanon McQuay <shanonmcquay@gmail.com>
2019-07-07 12:40:12 +1000

    Extract neovim config.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,18 +1,6 @@
 { config, pkgs, ... }:
 
-let customVimPlugins = {
-  nord = pkgs.vimUtils.buildVimPlugin {
-    name = "nord";
-    src = pkgs.fetchFromGitHub {
-      owner = "arcticicestudio";
-      repo = "nord-vim";
-      rev = "81d80e4a978aaab77e930b094bfb6780f695b438";
-      sha256 = "1s3jd8g7xnqn9pbg4s127ai9qm7zb6nvrmww842iz8qs3nmvf60v";
-    };
-  };
-};
-
-in {
+{
   system.stateVersion = "19.03";
 
   imports =
@@ -80,9 +68,9 @@ in {
   home-manager.users.shandogs = { pkgs, ... }: {
     imports = [
       ./home/alacritty.nix
+      ./home/neovim.nix
       ./home/zsh.nix
     ];
-
 
     programs.git = {
       enable = true;
@@ -95,118 +83,6 @@ in {
       };
     };
 
-    programs.neovim = {
-      enable = true;
-      vimAlias = true;
-      configure = {
-        customRC = ''
-          " ************************************************************
-          "  Display
-          " ************************************************************
-
-          " Show line numbers
-          set number
-
-          " Disable line wrapping
-          set nowrap
-
-          " Show dangling whitespace
-          set list listchars=tab:\ \ ,trail:·
-
-          " Disable code folding
-          set nofoldenable
-
-          " Use the nord colorscheme
-          colorscheme nord
-
-          " ************************************************************
-          " Editing
-          " ************************************************************
-
-          " Use spaces intead of tabs
-          set expandtab
-          set tabstop=2
-          set softtabstop=2
-          set shiftwidth=2
-
-          " Don't let vim manage backups
-          set nobackup
-          set nowritebackup
-          set noswapfile
-
-          " Allow buffers to be backgrounded without saving
-          set hidden
-
-          " ************************************************************
-          " Search
-          " ************************************************************
-
-          " Ignore case unless a capital letter is used
-          set ignorecase
-          set smartcase
-
-
-          " ************************************************************
-          " Leader key
-          " ************************************************************
-
-          " Use a leader key closer to the home row
-          let mapleader = ','
-
-
-          " ************************************************************
-          " Normal mode key bindings
-          " ************************************************************
-
-          " Quickly switch between split windows
-          noremap <C-h> <C-w>h
-          noremap <C-j> <C-w>j
-          noremap <C-k> <C-w>k
-          noremap <C-l> <C-w>l
-
-          " Make Y behave like D
-          nnoremap Y y$
-
-          " Quickly insert whitespace in normal mode
-          nnoremap <Space> i<Space><Esc>l
-
-          " Quickly save all files and re run the last command
-          nnoremap ! :wa<CR>:!!<CR>
-
-          " Quickly save all files and exit vim
-          nnoremap Q :wa<CR>ZZ
-
-          " Clear search highlights
-          nnoremap <Leader>, :nohlsearch<CR>
-
-
-          " ************************************************************
-          " Insert mode key bindings
-          " ************************************************************
-
-          " Quickly exit insert mode
-          inoremap kj <Esc> :update<CR>
-
-
-          " ************************************************************
-          " Visual mode key bindings
-          " ************************************************************
-
-          " Copy selection to the system clipboard
-          vnoremap <Leader>c "+y
-        '';
-        plug.plugins = with pkgs.vimPlugins // customVimPlugins; [
-          nord
-
-          vim-airline
-          vim-airline-themes
-          vim-nix
-          vim-surround
-          vim-repeat
-        ];
-      };
-
-    };
 
     programs.skim = {
       enable = true;
