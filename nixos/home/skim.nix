5ab936d45ebeacfb36ee3a0e93b84f8c045097ea
Shanon McQuay <shanonmcquay@gmail.com>
2019-07-18 19:17:54 +1000

    Add a temporary fix for an issue with skim options.

--- nixos/home/skim.nix
+++ nixos/home/skim.nix
@@ -1,13 +1,19 @@
 { pkgs, ... }:
 
 {
+  # Temporary workaround for an issue in the 19.03 branch.
+  home.sessionVariables = {
+    SKIM_DEFAULT_OPTIONS = "--reverse --height 40%";
+  };
+
   programs.skim = {
     enable = true;
     enableZshIntegration = true;
     defaultCommand = "${pkgs.fd}/bin/fd --type f";
-    defaultOptions = [
-      "--reverse"
-      "--height 40%"
-    ];
+    # Doesn't work in the 19.03 branch.
+    # defaultOptions = [
+    #   "--reverse"
+    #   "--height 40%"
+    # ];
   };
 }
