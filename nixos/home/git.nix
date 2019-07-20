9d9d13305228fdd8d78eeecedbb5ad1fb896e4ae
Shanon McQuay <shanonmcquay@gmail.com>
2019-07-20 15:10:22 +1000

    Move git aliases to love with the rest of the git configuration.

--- nixos/home/git.nix
+++ nixos/home/git.nix
@@ -11,4 +11,22 @@
       };
     };
   };
+
+  programs.zsh.shellAliases = {
+    g    = "${pkgs.git}/bin/git";
+    ga   = "${pkgs.git}/bin/git add";
+    gars = "${pkgs.git}/bin/git add . && git reset --hard";
+    gc   = "${pkgs.git}/bin/git commit";
+    gcm  = "${pkgs.git}/bin/git commit -m";
+    gca  = "${pkgs.git}/bin/git commit --amend";
+    gcp  = "${pkgs.git}/bin/git cherry-pick";
+    gco  = "${pkgs.git}/bin/git checkout";
+    gd   = "${pkgs.git}/bin/git diff";
+    gdc  = "${pkgs.git}/bin/git diff --cached";
+    glr  = "${pkgs.git}/bin/git pull --rebase";
+    glg  = "${pkgs.git}/bin/git log --stat";
+    grh  = "${pkgs.git}/bin/git reset HEAD";
+    grm  = "${pkgs.git}/bin/git rm";
+    gs   = "${pkgs.git}/bin/git status";
+  };
 }
--- nixos/home/zsh.nix
+++ nixos/home/zsh.nix
@@ -28,25 +28,11 @@
     };
 
     shellAliases = {
-      g    = "${pkgs.git}/bin/git";
-      ga   = "${pkgs.git}/bin/git add";
-      gars = "${pkgs.git}/bin/git add . && git reset --hard";
-      gc   = "${pkgs.git}/bin/git commit";
-      gcm  = "${pkgs.git}/bin/git commit -m";
-      gca  = "${pkgs.git}/bin/git commit --amend";
-      gcp  = "${pkgs.git}/bin/git cherry-pick";
-      gco  = "${pkgs.git}/bin/git checkout";
-      gd   = "${pkgs.git}/bin/git diff";
-      glr  = "${pkgs.git}/bin/git pull --rebase";
-      glg  = "${pkgs.git}/bin/git log --stat";
-      grh  = "${pkgs.git}/bin/git reset HEAD";
-      grm  = "${pkgs.git}/bin/git rm";
-      gs   = "${pkgs.git}/bin/git status";
-      l    = "${pkgs.exa}/bin/exa -la";
-      lt   = "${pkgs.exa}/bin/exa -l --tree";
-      b    = "${pkgs.bat}/bin/bat";
-      md   = "mkdir -p";
-      v    = "vim";
+      l  = "${pkgs.exa}/bin/exa -la";
+      lt = "${pkgs.exa}/bin/exa -l --tree";
+      b  = "${pkgs.bat}/bin/bat";
+      md = "mkdir -p";
+      v  = "vim";
     };
   };
 }
