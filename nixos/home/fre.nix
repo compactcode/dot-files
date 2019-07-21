51b1047f8f3f1889cfd1fb7dd4d73f7ccf35e507
Shanon McQuay <shanonmcquay@gmail.com>
2019-07-21 10:36:11 +1000

    Add the ability to auto jump to a directory.

--- nixos/home/fre.nix
+++ nixos/home/fre.nix
@@ -1,26 +1,50 @@
 { pkgs, ... }:
 
-{
+let fre = (pkgs.callPackage ../pkgs/fre.nix {});
+
+in {
   home.packages = [
-    (pkgs.callPackage ../pkgs/fre.nix {})
+    fre
   ];
 
   programs.zsh.initExtra = ''
-    # Record the current directory in the 'dirs' store.
     fre_chpwd() {
-      fre --add "$(pwd)" --store_name dirs
+      ${fre}/bin/fre --add "$(pwd)" --store_name dirs
     }
 
-    # Enable auto-execution of chpwd functions.
+    # List the contents of the 'dirs' store.
+    fre_dirs() {
+      ${fre}/bin/fre --sorted --store_name dirs
+    }
+
+    # Enable chpwd function hooks.
     typeset -ga chpwd_functions
 
-    # Enable auto recording.
+    # Enable auto storing of pwd when changing directories.
     chpwd_functions+=fre_chpwd
+
+    fre_jump() {
+      local search_pattern selected_directory
+
+      search_pattern=$1
+
+      if [[ -n $search_pattern ]]
+      then
+        # If an argument was provided then autojump.
+        selected_directory="$(fre_dirs | grep $search_pattern | head -1)"
+      else
+        # If no argument was provided then show a selection menu.
+        selected_directory="$(fre_dirs | ${pkgs.skim}/bin/sk --no-sort)"
+      fi
+
+      if [[ -n $selected_directory ]]
+      then
+        cd $selected_directory
+      fi
+    }
   '';
 
   programs.zsh.shellAliases = {
-    j = ''
-      cd "$(fre --sorted --store_name dirs | sk --no-sort)"
-    '';
+    j = "fre_jump";
   };
 }
