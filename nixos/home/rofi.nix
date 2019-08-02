9360afdd9c6b3662cefd6e7a36517a325f3507e9
Shanon McQuay <shanonmcquay@gmail.com>
2019-08-02 21:29:54 +1000

    Remove the hardcoded rofi theme.

--- nixos/home/rofi.nix
+++ nixos/home/rofi.nix
@@ -1,9 +1,29 @@
 { pkgs, ... }:
 
-{
+let theme = import ./themes/base_16_current.nix;
+
+in {
   programs.rofi = {
     enable = true;
 
-    theme = "Arc-Dark";
+    colors = {
+      window = {
+        background = theme.base00-hex;
+        border = theme.base05-hex;
+        separator = theme.base05-hex;
+      };
+
+      rows = {
+        normal = {
+          background = theme.base00-hex;
+          foreground = theme.base05-hex;
+          backgroundAlt = theme.base03-hex;
+          highlight = {
+            background = theme.base00-hex;
+            foreground = theme.base0A-hex;
+          };
+        };
+      };
+    };
   };
 }
