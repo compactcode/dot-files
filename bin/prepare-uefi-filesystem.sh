d2e82c5faca822ec09b94b6a5e730e551648d574
Shanon McQuay <shanonmcquay@gmail.com>
2019-08-05 21:10:08 +1000

    Remove hardcoded drive names since they will need to change for nvme drives.

--- bin/prepare-uefi-filesystem.sh
+++ bin/prepare-uefi-filesystem.sh
@@ -5,19 +5,22 @@ set -ex
 # The size of the boot partition.
 BOOT_SIZE=512MiB
 
-# Partition the disk.
-parted /dev/sda -- mklabel gpt
-# Boot partition    (sda1)
-parted /dev/sda -- mkpart ESP fat32 1MiB $BOOT_SIZE
+# The drive we will be installing on.
+DRIVE_NAME=/dev/sda
+
+# Partition using gpt as required by UEFI.
+parted $DRIVE_NAME -- mklabel gpt
+# Boot partition (sda1)
+parted $DRIVE_NAME -- mkpart ESP fat32 1MiB $BOOT_SIZE
 # Primary partition (sda2)
-parted /dev/sda -- mkpart primary $BOOT_SIZE 100%
+parted $DRIVE_NAME -- mkpart primary $BOOT_SIZE 100%
 # Enable the boot partition.
-parted /dev/sda -- set 1 boot on
+parted $DRIVE_NAME -- set 1 boot on
 
 # Format the boot partition.
-mkfs.fat -F 32 -n boot /dev/sda1
+mkfs.fat -F 32 -n boot ${DRIVE_NAME}1
 # Format the primary partition.
-mkfs.ext4 -L nixos /dev/sda2
+mkfs.ext4 -L nixos ${DRIVE_NAME}2
 
 # Wait for disk labels to be ready.
 sleep 4
