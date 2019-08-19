49175bee466beb42390e2f2525aa77f8ef28f642
Shanon McQuay <shanonmcquay@gmail.com>
2019-08-19 23:13:11 +1000

    Make a note to fix up sshcontrol.

--- nixos/home/desktop/security.nix
+++ nixos/home/desktop/security.nix
@@ -9,5 +9,9 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    # Doesn't work in the 19.03 branch.
+    # sshKeys = [
+    #   "D5340EDC116D6C8DFFE80518525712D7E2616FBB"
+    # ];
   };
 }
