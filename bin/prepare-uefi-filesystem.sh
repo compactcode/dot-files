60b6f35d93957625d9f97f8eb60a090c2bd22e8c
Shanon McQuay <shanonmcquay@gmail.com>
2019-08-31 14:03:29 +1000

    Move the drive and secret into environment variables.

--- bin/prepare-uefi-filesystem.sh
+++ bin/prepare-uefi-filesystem.sh
@@ -1,29 +1,23 @@
 #!/bin/sh
 
-set -ex
+set -eux
 
 # The size of the boot partition.
 BOOT_SIZE=512MiB
 
-# The drive we will be installing on.
-DRIVE_NAME=/dev/sda
-
-# The encryption password for drive we will be installing on.
-DRIVE_PASSWORD='secret'
-
 # Partition using gpt as required by UEFI.
-parted $DRIVE_NAME -- mklabel gpt
+parted $INSTALL_DRIVE_NAME -- mklabel gpt
 # Boot partition
-parted $DRIVE_NAME -- mkpart ESP fat32 1MiB $BOOT_SIZE
+parted $INSTALL_DRIVE_NAME -- mkpart ESP fat32 1MiB $BOOT_SIZE
 # Primary partition
-parted $DRIVE_NAME -- mkpart primary $BOOT_SIZE 100%
+parted $INSTALL_DRIVE_NAME -- mkpart primary $BOOT_SIZE 100%
 # Enable the boot partition.
-parted $DRIVE_NAME -- set 1 boot on
+parted $INSTALL_DRIVE_NAME -- set 1 boot on
 
 # Setup encryption on the primary partition.
-echo $DRIVE_PASSWORD | cryptsetup luksFormat /dev/disk/by-partlabel/primary
+echo $INSTALL_DRIVE_PASSWORD | cryptsetup luksFormat /dev/disk/by-partlabel/primary
 # Mount a decrypted version of the encrypted primary partition.
-echo $DRIVE_PASSWORD | cryptsetup luksOpen /dev/disk/by-partlabel/primary nixos-decrypted
+echo $INSTALL_DRIVE_PASSWORD | cryptsetup luksOpen /dev/disk/by-partlabel/primary nixos-decrypted
 
 # Format the boot partition.
 mkfs.fat -F 32 -n boot /dev/disk/by-partlabel/ESP
