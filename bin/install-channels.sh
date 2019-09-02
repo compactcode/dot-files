a2ee592448fc7e87b388480b37c636e84bdd92d2
Shanon McQuay <hi@shan.dog>
2019-09-02 20:10:36 +1000

    Remove the now unused channel.

--- bin/install-channels.sh
+++ bin/install-channels.sh
@@ -8,9 +8,6 @@ nix-channel --add https://nixos.org/channels/nixos-19.03 nixos
 # Include the unstable channel so we can upgrade specific packages.
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
 
-# Include the hardware channel for dealing with hardware quirks.
-nix-channel --add https://github.com/NixOS/nixos-hardware/archive/master.tar.gz nixos-hardware
-
 # Include home manager for setting up user environments.
 nix-channel --add https://github.com/rycee/home-manager/archive/release-19.03.tar.gz home-manager
 
