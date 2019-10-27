5ee65eb362e9833bbde037c658946b943fa71074
Shanon McQuay <hi@shan.dog>
2019-10-27 18:32:11 +1100

    Remove old skim config.

--- nixos/home/terminal/basic/skim.nix
+++ nixos/home/terminal/basic/skim.nix
@@ -1,19 +1,13 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
-  # Temporary workaround for an issue in the 19.03 branch.
-  home.sessionVariables = {
-    SKIM_DEFAULT_OPTIONS = "--reverse --height 40%";
-  };
-
   programs.skim = {
     enable = true;
     enableZshIntegration = true;
-    defaultCommand = "${pkgs.fd}/bin/fd --type f";
-    # Doesn't work in the 19.03 branch.
-    # defaultOptions = [
-    #   "--reverse"
-    #   "--height 40%"
-    # ];
+    defaultCommand = "${lib.getBin pkgs.fd}/bin/fd --type f";
+    defaultOptions = [
+      "--reverse"
+      "--height 40%"
+    ];
   };
 }
