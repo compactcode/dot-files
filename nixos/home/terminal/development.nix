2ccd618f534dd6bd9919bca01eae8c1634d10916
Shanon McQuay <hi@shan.dog>
2019-12-10 21:24:12 +1100

    Add aws-vault for securing AWS credentials.

--- nixos/home/terminal/development.nix
+++ nixos/home/terminal/development.nix
@@ -1,6 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    # Secure AWS credential handling.
+    aws-vault
+  ];
+
   imports = [
     ./development/direnv.nix
     ./development/git.nix
