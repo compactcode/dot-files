6f99ec95a6f5b6295dccdf3d1057c86e2ef1d103
Shanon McQuay <hi@shan.dog>
2020-02-28 09:44:52 +1100

    Add flatpak apps

---
+++ flatpak.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.github.alecaddd.sequeler
+flatpak install flathub com.slack.Slack
+flatpak install flathub us.zoom.Zoom
--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 sh ./apt.sh
 sh ./snap.sh
+sh ./flatpak.sh
 sh ./homebrew.sh
 sh ./link.sh
 sh ./finalise.sh
