5f1be7fad5370944226d831b090f56e262cbc342
Shanon McQuay <hi@shan.dog>
2020-02-29 18:33:49 +1100

    Add redis

--- apt.sh
+++ apt.sh
@@ -3,6 +3,7 @@
 set -ex
 
 sudo apt-get update
+
 sudo apt-get install -y build-essential
 sudo apt-get install -y chromium-browser
 sudo apt-get install -y chromium-chromedriver
@@ -11,5 +12,6 @@ sudo apt-get install -y firefox
 sudo apt-get install -y git
 sudo apt-get install -y postgresql-10
 sudo apt-get install -y postgresql-server-dev-10
+sudo apt-get install -y redis-server
 sudo apt-get install -y snapd
 sudo apt-get install -y zsh
