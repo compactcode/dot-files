73d0eb551744c9d379a574fb35ffa649122c5adc
Shanon McQuay <hi@shan.dog>
2020-02-29 17:36:21 +1100

    Add chrome & chrome driver

--- apt.sh
+++ apt.sh
@@ -4,10 +4,12 @@ set -ex
 
 sudo apt-get update
 sudo apt-get install -y build-essential
+sudo apt-get install -y chromium-browser
+sudo apt-get install -y chromium-chromedriver
 sudo apt-get install -y curl
 sudo apt-get install -y firefox
 sudo apt-get install -y git
-sudo apt-get install -y snapd
 sudo apt-get install -y postgresql-10
 sudo apt-get install -y postgresql-server-dev-10
+sudo apt-get install -y snapd
 sudo apt-get install -y zsh
