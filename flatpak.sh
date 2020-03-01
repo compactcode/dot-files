19851b651acba433fda18f7cc7638585705574db
Shanon McQuay <hi@shan.dog>
2020-03-01 18:37:02 +1100

    Add libre for office documents

--- flatpak.sh
+++ flatpak.sh
@@ -5,4 +5,5 @@ set -ex
 flatpak install flathub com.discordapp.Discord
 flatpak install flathub com.github.alecaddd.sequeler
 flatpak install flathub com.slack.Slack
+flatpak install flathub org.libreoffice.LibreOffice
 flatpak install flathub us.zoom.Zoom
