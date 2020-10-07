6c3c71b1ecd28a08e973104bc02cd1ed5ed0714b
Shanon McQuay <hi@shan.dog>
2020-10-08 09:07:03 +1100

    No need for custom ranger anymore.

--- nixos/pkgs/default.nix
+++ nixos/pkgs/default.nix
@@ -1,8 +1,5 @@
 (
   self: super: {
     fre = super.callPackage ./fre.nix {};
-
-    # 1.9.2 ranger doesn't handle padding on the terminal emulator window.
-    ranger = super.callPackage ./ranger.nix {};
   }
 )
