cd5fb40538b926d96c524b11b2bc7abf4f4ea8a0
Shanon McQuay <hi@shan.dog>
2021-01-12 14:03:57 +1100

    Exclude video call apps from opacity.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -113,6 +113,8 @@ in {
       inactiveOpacity = 0.950;
       opacityRules = [
         "100:class_g = 'Firefox'"
+        "100:class_g = 'discord'"
+        "100:class_g = 'zoom'"
       ];
       shadow = true;
       vSync = true;
