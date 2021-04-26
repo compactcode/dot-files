fab0ce8d290711194cb4895ec0e1fdd87b473a1f
Shanon McQuay <hi@shan.dog>
2021-04-26 21:14:52 +1000

    Update the fre package.

--- nixos/pkgs/fre.nix
+++ nixos/pkgs/fre.nix
@@ -4,11 +4,11 @@ rustPlatform.buildRustPackage {
   name = "fre";
 
   src = fetchFromGitHub {
-    owner = "ccheek21";
+    owner = "camdencheek";
     repo = "fre";
     rev =  "32c8e729a43a654e355b19f73b1900b9857076bf";
     sha256 = "1xc32q4b400aqsshydnj7cn3jn3fp3v9glb7qkc6nvc4wjn3m1fm";
   };
 
-  cargoSha256 = "0jsv059cw1h0wa9mab0q7qifj1i76rqi0b214hzyvz3iphdss9ak";
+  cargoSha256 = "0rxhlj6da3krb667dr03ss0m3jricyk7vsl11p3yagj67c5jnd3p";
 }
