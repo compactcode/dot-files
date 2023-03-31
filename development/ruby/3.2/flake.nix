e3407944a9f42a29c07e9597c229204abe722505
Shanon McQuay <hi@shan.dog>
2023-03-31 13:55:04 +1100

    feat: flake for a basic ruby project

---
+++ development/ruby/3.2/flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-ruby = {
+      url = "github:bobvanderlinden/nixpkgs-ruby";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-ruby, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        ruby = nixpkgs-ruby.packages.${system}."ruby-3.2.1";
+      in {
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              ruby
+            ];
+
+            shellHook = ''
+              export GEM_HOME="./tmp/gem/ruby"
+              export GEM_PATH=$GEM_HOME
+              export PATH=$GEM_HOME/bin:$PATH
+              export PATH=$PWD/bin:$PATH
+            '';
+          };
+      });
+}
