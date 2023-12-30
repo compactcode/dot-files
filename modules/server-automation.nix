3a32726170053b8a8e6ab9b22cbdd8faa285dd98
Shanon McQuay <hi@shan.dog>
2023-12-30 15:55:12 +1100

    fix: invalid env variable

--- modules/server-automation.nix
+++ modules/server-automation.nix
@@ -4,14 +4,14 @@
   virtualisation.oci-containers = {
     backend = "docker";
     containers.homeassistant = {
-      extraOptions = [
-        "--network=host"
-      ];
       environment = {
         TZ = "Australia/Melbourne";
         PUID = "1001"; # service user
-        GUID = "1001"; # service group
+        PGID = "1001"; # service group
       };
+      extraOptions = [
+        "--network=host"
+      ];
       image = "lscr.io/linuxserver/homeassistant:2023.12.4";
       ports = [
         "8123:8123"
