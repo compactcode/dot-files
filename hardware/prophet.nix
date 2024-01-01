1549aab880ca598615c3a10cce8d265aae88802b
Shanon McQuay <hi@shan.dog>
2024-01-01 16:23:16 +1100

    fix(prophet): disable dedicated gpu

--- hardware/prophet.nix
+++ hardware/prophet.nix
@@ -6,6 +6,16 @@
 
 {
   boot = {
+    # Disable Nvidia GPU
+    blacklistedKernelModules =
+      [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+
+    # Disable Nvidia GPU
+    extraModprobeConfig = ''
+      blacklist nouveau
+      options nouveau modeset=0
+    '';
+
     initrd = {
       availableKernelModules =
         [ "ahci" "nvme" "sd_mod" "usb_storage" "usbhid" "xhci_pci" ];
@@ -60,6 +70,17 @@
 
   # set a hostname
   networking.hostName = "prophet";
+
+  services.udev.extraRules = ''
+    # Remove NVIDIA USB xHCI Host Controller devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA USB Type-C UCSI devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA Audio devices, if present
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+    # Remove NVIDIA VGA/3D controller devices
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+  '';
 
   # set a swap file
   swapDevices = [{ device = "/swap/swapfile"; }];
