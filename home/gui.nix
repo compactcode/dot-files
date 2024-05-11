7aad3ab459e1a4b62cc14fb2018f86a5f920fbf5
Shanon McQuay <hi@shan.dog>
2024-05-11 20:28:09 +1000

    feat: add basic keybinds for moving around

--- home/gui.nix
+++ home/gui.nix
@@ -62,10 +62,27 @@
     settings = {
       "$mod" = "SUPER";
       bind = [
-        "$mod, n, exec, rofi -show drun"
         "$mod, e, exec, kitty"
+        "$mod, f, fullscreen,"
         "$mod, i, exec, firefox"
         "$mod, k, exit,"
+        "$mod, n, exec, rofi -show drun"
+
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
       ];
     };
     # hyprland-session.target
