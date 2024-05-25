aa6c6c367536c14c2c58b2f84465da613b85da66
Shanon McQuay <hi@shan.dog>
2024-05-25 14:37:29 +1000

    refactor(shell): cleanup zepto shell

--- development/zepto/main/flake.nix
+++ development/zepto/main/flake.nix
@@ -11,79 +11,77 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
-    {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
 
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
 
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = with pkgs; [
-                    # api browser
-                    bruno
-                    slack
-                    # cabybara js driver
-                    chromedriver
-                    chromium
-                    # psych(ruby-lsp)
-                    libyaml
-                  ];
-
-                  languages = {
-                    javascript = {
-                      enable = true;
-                      package = pkgs.nodejs_18;
-                      yarn.enable = true;
-                    };
-
-                    python = {
-                      enable = true;
-                      package = pkgs.python310;
-                      venv.enable = true;
-                    };
-
-                    ruby = {
-                      enable = true;
-                      package = pkgs.ruby;
-                    };
-                  };
-
-                  services = {
-                    redis = {
-                      enable = true;
-                      package = pkgs.redis;
-                    };
-
-                    postgres = {
-                      enable = true;
-                      package = pkgs.postgresql_12;
-                      initialDatabases = [
-                        {name = "split_development";}
-                        {name = "split_test";}
-                      ];
-                      initialScript = ''
-                        CREATE USER postgres SUPERUSER;
-                      '';
-
-                    };
-                  };
-                }
+          modules = [
+            {
+              # https://devenv.sh/reference/options/
+              packages = with pkgs; [
+                # cabybara js driver
+                chromedriver
+                chromium
+                # psych(ruby-lsp)
+                libyaml
               ];
-            };
-          });
-    };
+
+              languages = {
+                javascript = {
+                  enable = true;
+                  package = pkgs.nodejs_18;
+                  yarn.enable = true;
+                };
+
+                ruby = {
+                  enable = true;
+                  package = pkgs.ruby;
+                };
+              };
+
+              services = {
+                redis = {
+                  enable = true;
+                  package = pkgs.redis;
+                };
+
+                postgres = {
+                  enable = true;
+                  package = pkgs.postgresql_12;
+                  initialDatabases = [
+                    {name = "split_development";}
+                    {name = "split_test";}
+                  ];
+                  initialScript = ''
+                    CREATE USER postgres SUPERUSER;
+                  '';
+                };
+              };
+
+              scripts.load-secrets.exec = ''
+                export BUNDLE_RUBYGEMS__PKG__GITHUB__COM=$(op read op://personal/gh-packages-prophet/token)
+                export BUNDLE_ENTERPRISE__CONTRIBSYS__COM=$(op read op://personal/sidekiq-ent/token)
+              '';
+            }
+          ];
+        };
+      });
+  };
 }
