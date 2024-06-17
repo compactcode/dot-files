230147fac963f6c5f56c5ab73618cea49d77296c
Shanon McQuay <hi@shan.dog>
2024-06-18 09:10:08 +1000

    feat(zepto): add a service for running cloudflare warp

--- modules/work/zepto.nix
+++ modules/work/zepto.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{lib, pkgs, ...}: {
   environment = {
     systemPackages = with pkgs; [
       cloudflare-warp # vpn
@@ -10,5 +10,19 @@
       127.0.0.1 api.split.test
       127.0.0.1 go.split.test
     '';
+  };
+
+  systemd = {
+    services = {
+      cloudflare-warp = {
+        description = "cloudflare warp vpn";
+        serviceConfig = {
+          AmbientCapabilities = ["CAP_NET_ADMIN" "CAP_NET_BIND_SERVICE" "CAP_SYS_PTRACE"];
+          CapabilityBoundingSet = ["CAP_NET_ADMIN" "CAP_NET_BIND_SERVICE" "CAP_SYS_PTRACE"];
+          ExecStart = "${lib.getExe' pkgs.cloudflare-warp "warp-svc"}";
+          Type = "simple";
+        };
+      };
+    };
   };
 }
