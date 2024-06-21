47b2f9cbe19192859e4dbcd7abcf5aeca839092f
Shanon McQuay <hi@shan.dog>
2024-06-21 20:03:50 +1000

    refactor(pudge): move configuration to flakelight

--- flake.nix
+++ flake.nix
@@ -36,27 +36,5 @@
   outputs = {flakelight, ...} @ inputs:
     flakelight.lib.mkFlake ./. {
       nixpkgs.config = {allowUnfree = true;};
-
-      nixosConfigurations = {
-        pudge = {
-          system = "x86_64-linux";
-          modules = [
-            ./hardware/pudge.nix
-            ./modules/server.nix
-            ./modules/server-automation.nix
-            ./modules/server-media.nix
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.shandogs = {
-                imports = [
-                  ./modules/server-home.nix
-                ];
-              };
-            }
-          ];
-        };
-      };
     };
 }
