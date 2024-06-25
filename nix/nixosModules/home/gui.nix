f26c42c4744601e026cef867295efde06338be21
Shanon McQuay <hi@shan.dog>
2024-06-26 07:54:10 +1000

    refactor(gui): extract waybar to a separate file

--- nix/nixosModules/home/gui.nix
+++ nix/nixosModules/home/gui.nix
@@ -6,8 +6,9 @@
 }: {
   imports = [
     ./gui/email
-    ./gui/wayland/rofi
     ./gui/wayland/hyprland
+    ./gui/wayland/rofi
+    ./gui/wayland/waybar
   ];
 
   # gtk syles
@@ -34,77 +35,6 @@
 
     # screen locker
     swaylock.enable = true;
-
-    # status bar
-    waybar = {
-      enable = true;
-      settings = {
-        mainBar = {
-          layer = "top"; # display on top of other windows
-          spacing = 5;
-
-          modules-left = [
-            "hyprland/workspaces" # active workspaces
-            "hyprland/submap" # active keybind mode
-          ];
-          modules-center = [
-            "clock"
-          ];
-          modules-right = [
-            "temperature"
-            "network"
-            "pulseaudio" # sound
-            "tray" # system tray
-          ];
-
-          battery = {
-            interval = 60;
-            states = {
-              warning = 30;
-              critical = 1;
-            };
-            "format" = "{capacity}% {icon}";
-            "format-icons" = ["" "" "" "" ""];
-            "max-length" = 2;
-          };
-
-          clock = {
-            format = "{:%H.%M}";
-            tooltip = true;
-            tooltip-format = "{:%d.%m.%Y}";
-          };
-
-          network = {
-            format = "{bandwidthUpBytes} {bandwidthDownBytes} 󰈀";
-            interval = 10;
-          };
-
-          pulseaudio = {
-            format = "{volume}% {icon} {format_source}";
-            format-muted = "󰝟 {format_source}";
-            format-icons = {
-              headphone = "󰋋";
-              muted-icon = "󰝟";
-              default = ["󰕿" "󰖀" "󰕾"];
-            };
-            format-source = "{volume}% 󰍬";
-            format-source-muted = " 󰍭";
-            on-click = "pavucontrol";
-            scroll-step = 5;
-          };
-
-          temperature = {
-            format = "{temperatureC}°C 󰻠";
-          };
-
-          tray = {
-            spacing = 5;
-          };
-        };
-      };
-
-      systemd.enable = true;
-    };
 
     # pdf viewer
     zathura.enable = true;
---
+++ nix/nixosModules/home/gui/wayland/waybar/default.nix
@@ -0,0 +1,74 @@
+{...}: {
+  programs = {
+    # status bar
+    waybar = {
+      enable = true;
+      settings = {
+        mainBar = {
+          layer = "top"; # display on top of other windows
+          spacing = 5;
+
+          modules-left = [
+            "hyprland/workspaces" # active workspaces
+            "hyprland/submap" # active keybind mode
+          ];
+          modules-center = [
+            "clock"
+          ];
+          modules-right = [
+            "temperature"
+            "network"
+            "pulseaudio" # sound
+            "tray" # system tray
+          ];
+
+          battery = {
+            interval = 60;
+            states = {
+              warning = 30;
+              critical = 1;
+            };
+            "format" = "{capacity}% {icon}";
+            "format-icons" = ["" "" "" "" ""];
+            "max-length" = 2;
+          };
+
+          clock = {
+            format = "{:%H.%M}";
+            tooltip = true;
+            tooltip-format = "{:%d.%m.%Y}";
+          };
+
+          network = {
+            format = "{bandwidthUpBytes} {bandwidthDownBytes} 󰈀";
+            interval = 10;
+          };
+
+          pulseaudio = {
+            format = "{volume}% {icon} {format_source}";
+            format-muted = "󰝟 {format_source}";
+            format-icons = {
+              headphone = "󰋋";
+              muted-icon = "󰝟";
+              default = ["󰕿" "󰖀" "󰕾"];
+            };
+            format-source = "{volume}% 󰍬";
+            format-source-muted = " 󰍭";
+            on-click = "pavucontrol";
+            scroll-step = 5;
+          };
+
+          temperature = {
+            format = "{temperatureC}°C 󰻠";
+          };
+
+          tray = {
+            spacing = 5;
+          };
+        };
+      };
+
+      systemd.enable = true;
+    };
+  };
+}
