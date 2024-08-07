7458e4cdcfc0c8ea3f11516d5460e05d264fa01a
Shanon McQuay <hi@shan.dog>
2024-08-07 13:00:09 +1000

    feat(nixvim): add mini.align for text alignment

--- nix/nixosModules/home/nixvim/plugins/mini.nix
+++ nix/nixosModules/home/nixvim/plugins/mini.nix
@@ -5,6 +5,7 @@
       enable = true;
       modules = {
         ai = {}; # text objects
+        align = {}; # text alignment
         indentscope = {}; # indent decorations
         pairs = {}; # auto pairs
         surround = {}; # surround actions
