95dae6467e265a12e52a9891d37f26ea33276221
Shanon McQuay <hi@shan.dog>
2024-08-23 11:06:47 +1000

    feat(email): initial tagging

--- nix/nixosModules/home/gui/email/default.nix
+++ nix/nixosModules/home/gui/email/default.nix
@@ -49,7 +49,22 @@
       hooks = {
         # process new staging tags
         postNew = ''
-          notmuch tag +inbox +unread -new -- tag:new
+          notmuch tag +sent -unread -new -- tag:new from:hi@shan.dog
+
+          notmuch tag +newsletter -- tag:new from:campaign.realestate.com.au
+          notmuch tag +newsletter -- tag:new from:rainbowplantlife.com
+          notmuch tag +newsletter -- tag:new from:rw@peterc.org
+          notmuch tag +newsletter -- tag:new from:tldrnewsletter.com
+
+          notmuch tag +shares -- tag:new from:linkmarketservices.com.au
+          notmuch tag +shares -- tag:new from:openmarkets.com.au
+
+          notmuch tag +bills -- tag:new from:amaysim.com.au
+          notmuch tag +bills -- tag:new from:team.aussiebroadband.com.au
+
+          notmuch tag +childcare -- tag:new from:educa.com.au
+
+          notmuch tag +inbox -new -- tag:new
         '';
       };
     };
