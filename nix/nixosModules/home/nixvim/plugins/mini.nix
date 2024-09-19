b80eb4f8682d11e5fa48638575cac97c17ebd160
Shanon McQuay <hi@shan.dog>
2024-09-19 16:05:23 +1000

    feat(nixvim): add treesitter text objects

--- nix/nixosModules/home/nixvim/plugins/mini.nix
+++ nix/nixosModules/home/nixvim/plugins/mini.nix
@@ -4,7 +4,27 @@
     plugins.mini = {
       enable = true;
       modules = {
-        ai = {}; # text objects
+        # text objects
+        ai = {
+          n_lines = 500;
+          custom_textobjects = {
+            # manipulate a class
+            c.__raw = ''
+              require("mini.ai").gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" })
+            '';
+            # manipulate a function
+            f.__raw = ''
+              require("mini.ai").gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" })
+            '';
+            # manipulate a code block
+            o.__raw = ''
+              require("mini.ai").gen_spec.treesitter({
+                a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+              })
+            '';
+          };
+        };
         align = {}; # text alignment
         indentscope = {}; # indent decorations
         pairs = {}; # auto pairs
--- nix/nixosModules/home/nixvim/plugins/treesitter.nix
+++ nix/nixosModules/home/nixvim/plugins/treesitter.nix
@@ -1,15 +1,25 @@
 {...}: {
   programs.nixvim = {
     # language parsing
-    plugins.treesitter = {
-      enable = true;
-      # highlight embedded lua
-      nixvimInjections = true;
-      settings = {
-        # replace default highlighting
-        highlight.enable = true;
-        # replace default indenting
-        indent.enable = true;
+    plugins = {
+      treesitter = {
+        enable = true;
+        # highlight embedded lua
+        nixvimInjections = true;
+        settings = {
+          # replace default highlighting
+          highlight.enable = true;
+          # replace default indenting
+          indent.enable = true;
+        };
+      };
+      # language query extensions (used by mini.ai)
+      treesitter-textobjects = {
+        enable = true;
+        settings = {
+          # use mini.ai instead
+          select.enable = false;
+        };
       };
     };
   };
