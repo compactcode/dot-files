1346108553df071a5d6eb0b1dadf596a6f6f14e6
Shanon McQuay <hi@shan.dog>
2024-09-21 11:11:52 +1000

    fix(cli): email sync flag using invalid options

--- nix/nixosModules/home/gui/email/scripts/sync.sh
+++ nix/nixosModules/home/gui/email/scripts/sync.sh
@@ -6,10 +6,13 @@ set -e
 notmuch search --format=text0 --output=files tag:killed | xargs -0 --no-run-if-empty rm
 
 # synchronise local maildirs with remote servers
-mbsync -all --pull --push
+mbsync --all
 
 # synchronise local maildirs with notmuch and tag new mail
 notmuch new
 
+# flush automated deletes to local maildirs
+notmuch search --format=text0 --output=files tag:killed | xargs -0 --no-run-if-empty rm
+
 # synchronise local tags with remote servers
-mbsync -all --push --flags
+mbsync --all --push --flags
