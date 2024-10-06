0d9e90ea14b81fad70ef7a1466159b977b4d2ab8
Shanon McQuay <hi@shan.dog>
2024-10-06 13:11:23 +1100

    feat(prophet): enable gpu acceleration + ssd trim

--- nix/nixosConfigurations/prophet/hardware.nix
+++ nix/nixosConfigurations/prophet/hardware.nix
@@ -3,10 +3,10 @@
 # Motherboard: ThinkPad P43s
 {
   boot = {
-    # Disable Nvidia GPU
+    # disable nvidia gpu
     blacklistedKernelModules = ["nouveau" "nvidia" "nvidia_drm" "nvidia_modeset"];
 
-    # Disable Nvidia GPU
+    # disable nvidia gpu
     extraModprobeConfig = ''
       blacklist nouveau
       options nouveau modeset=0
@@ -25,16 +25,26 @@
     };
     # enable microcode updates
     cpu.intel.updateMicrocode = true;
+    # enable gpu acceleration
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
   };
 
-  services.udev.extraRules = ''
-    # Remove NVIDIA USB xHCI Host Controller devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA USB Type-C UCSI devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA Audio devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA VGA/3D controller devices
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-  '';
+  services = {
+    # periodic ssd maintenance
+    fstrim.enable = true;
+    # disable nvidia gpu
+    udev.extraRules = ''
+      # Remove NVIDIA USB xHCI Host Controller devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA USB Type-C UCSI devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA Audio devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+      # Remove NVIDIA VGA/3D controller devices
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+    '';
+  };
 }
