43cd5145ea1cc04f4b330f2422fff282b3614993
Shanon McQuay <hi@shan.dog>
2024-10-08 09:47:39 +1100

    feat(cli): configure yazi mime types

--- nix/nixosModules/home/cli/core.nix
+++ nix/nixosModules/home/cli/core.nix
@@ -102,6 +102,52 @@
     yazi = {
       enable = true;
       enableZshIntegration = true;
+      settings = {
+        # open options per mime type
+        open.rules = [
+          {
+            mime = "application/pdf";
+            use = ["xdg"];
+          }
+          {
+            mime = "application/zip";
+            use = ["ouch"];
+          }
+          {
+            mime = "image/*";
+            use = ["xdg"];
+          }
+          {
+            mime = "text/html";
+            use = ["xdg" "edit"];
+          }
+          {
+            mime = "video/*";
+            use = ["xdg"];
+          }
+          {
+            mime = "*";
+            use = ["edit"];
+          }
+        ];
+
+        opener = {
+          # use xdg configuration
+          xdg = [
+            {
+              run = ''xdg-open "$@"'';
+              desc = "Open";
+            }
+          ];
+          # use ouch to extract archive
+          ouch = [
+            {
+              run = ''ouch decompress "$@"'';
+              desc = "Extract";
+            }
+          ];
+        };
+      };
     };
 
     # smart cd with jumping
