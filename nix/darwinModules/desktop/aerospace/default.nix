153d44260100a1f80e89627a1e26ede72e1b8da8
Shanon McQuay <hi@shan.dog>
2024-12-06 10:40:16 +1100

    feat(alchemist): add more aerospace key binds

--- nix/darwinModules/desktop/aerospace/default.nix
+++ nix/darwinModules/desktop/aerospace/default.nix
@@ -35,6 +35,20 @@
       alt-shift-ctrl-cmd-m = 'exec-and-forget open -a /System/Applications/Mail.app'
       alt-shift-ctrl-cmd-n = 'exec-and-forget open -a /Applications/Google\ Chrome.app'
       alt-shift-ctrl-cmd-o = 'exec-and-forget open -a ${pkgs.obsidian}/Applications/Obsidian.app'
+
+      alt-shift-ctrl-1 = 'move-node-to-workspace 1'
+      alt-shift-ctrl-2 = 'move-node-to-workspace 2'
+      alt-shift-ctrl-3 = 'move-node-to-workspace 3'
+      alt-shift-ctrl-4 = 'move-node-to-workspace 4'
+      alt-shift-ctrl-5 = 'move-node-to-workspace 5'
+
+      alt-shift-ctrl-a = 'layout accordion horizontal vertical'
+      alt-shift-ctrl-t = 'layout tiles horizontal vertical'
+
+      alt-shift-ctrl-m = 'focus left'
+      alt-shift-ctrl-n = 'focus down'
+      alt-shift-ctrl-e = 'focus up'
+      alt-shift-ctrl-i = 'focus right'
     '';
   };
 }
