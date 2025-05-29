0c44f3a19c54774566444c6ab3f29a3c26d4d2fd
Shanon McQuay <hi@shan.dog>
2025-05-30 09:10:40 +1000

    refactor(nixvim): lazy load oil

--- nix/homeModules/nixvim/plugins/oil.nix
+++ nix/homeModules/nixvim/plugins/oil.nix
@@ -1,16 +1,9 @@
 {...}: {
   programs.nixvim = {
-    keymaps = [
-      {
-        key = "<leader>e";
-        action = "<cmd>Oil<cr>";
-        options = {desc = "explore files";};
-      }
-    ];
-
     # file explorer
     plugins.oil = {
       enable = true;
+
       settings = {
         # dont prompt when making non destructive changes
         skip_confirm_for_simple_edits = true;
@@ -18,6 +11,18 @@
           # show files and directories starting with "."
           show_hidden = true;
         };
+      };
+
+      # delay loading until requested
+      lazyLoad.settings = {
+        cmd = "Oil";
+        keys = [
+          {
+            __unkeyed-1 = "<leader>e";
+            __unkeyed-2 = "<cmd>Oil<cr>";
+            desc = "explore files";
+          }
+        ];
       };
     };
   };
