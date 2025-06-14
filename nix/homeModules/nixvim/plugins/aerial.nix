64b48ed413ec399d21483583066a6b7bf04364ce
Shanon McQuay <hi@shan.dog>
2025-06-14 14:37:28 +1000

    refactor(nixvim): lazy load aerial
    currently not working due to a bug

--- nix/homeModules/nixvim/plugins/aerial.nix
+++ nix/homeModules/nixvim/plugins/aerial.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   programs.nixvim = {
     # enable colorscheme
     colorschemes.catppuccin = {
@@ -9,21 +9,25 @@
       };
     };
 
-    keymaps = [
-      {
-        key = "<leader>ss";
-        action = "<cmd>Telescope aerial<cr>";
-        options = {desc = "code symbol search";};
-      }
-      {
-        key = "<leader>so";
-        action = "<cmd>AerialToggle<cr>";
-        options = {desc = "code symbol outline";};
-      }
-    ];
-
     plugins.aerial = {
       enable = true;
+
+      # delay loading until requested
+      lazyLoad.settings = {
+        cmd = ["AerialInfo" "AerialOpen" "AerialToggle"];
+        keys = [
+          {
+            __unkeyed-1 = "<leader>ss";
+            __unkeyed-2 = "<cmd>Telescope aerial<cr>";
+            desc = "code symbol search";
+          }
+          {
+            __unkeyed-1 = "<leader>so";
+            __unkeyed-2 = "<cmd>AerialToggle<cr>";
+            desc = "code symbol outline";
+          }
+        ];
+      };
     };
   };
 }
