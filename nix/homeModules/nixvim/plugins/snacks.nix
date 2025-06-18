cdaf6c0f81762c445373d91be956bda8ea761bfc
Shanon McQuay <hi@shan.dog>
2025-06-18 20:23:51 +1000

    Revert "feat(nixvim): replace gitlinker with snacks git browse"
    This reverts commit 03b59e08fb6cc3bff578583e0818138bedf49dd9.

--- nix/homeModules/nixvim/plugins/snacks.nix
+++ nix/homeModules/nixvim/plugins/snacks.nix
@@ -1,22 +1,9 @@
 {...}: {
   programs.nixvim = {
-    keymaps = [
-      {
-        key = "<leader>go";
-        action = "<cmd>lua Snacks.gitbrowse()<cr>";
-        mode = ["n" "v"];
-        options = {desc = "open permalink url to current line(s)";};
-      }
-    ];
-
     # collection of plugins
     plugins.snacks = {
       enable = true;
       settings = {
-        # git permalinks
-        gitbrowse = {
-          enable = true;
-        };
         # upgrade input prompt
         input = {
           enable = true;
