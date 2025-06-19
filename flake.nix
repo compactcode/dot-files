f869a8390001d0935df7f8dcdb188284e23f95e4
Shanon McQuay <hi@shan.dog>
2025-06-20 09:36:12 +1000

    chore(deps): update nix-darwin to new repo

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
