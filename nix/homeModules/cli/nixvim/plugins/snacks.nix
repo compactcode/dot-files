9f145f68a00af076083508d86db8b374f54609c3
Shanon McQuay <hi@shan.dog>
2025-06-27 09:31:22 +1000

    refactor(nixvim): lazy load snacks

--- nix/homeModules/cli/nixvim/plugins/snacks.nix
+++ nix/homeModules/cli/nixvim/plugins/snacks.nix
@@ -3,11 +3,17 @@
     # collection of plugins
     plugins.snacks = {
       enable = true;
+
       settings = {
         # upgrade input prompt
         input = {
           enable = true;
         };
+      };
+
+      # delay loading until the ui is loaded
+      lazyLoad.settings = {
+        event = "DeferredUIEnter";
       };
     };
   };
