93cfd7db455900493493eada6004bcc5a5c9f584
Shanon McQuay <hi@shan.dog>
2025-06-27 13:42:48 +1000

    feat(nixvim): add codecompanion keybinds

--- nix/homeModules/cli/nixvim/plugins/ai/codecompanion.nix
+++ nix/homeModules/cli/nixvim/plugins/ai/codecompanion.nix
@@ -1,11 +1,34 @@
 {...}: {
   programs.nixvim = {
+    keymaps = [
+      {
+        key = "<leader>aa";
+        action = "<cmd>CodeCompanionChat Add<cr>";
+        mode = ["v"];
+        options = {desc = "add selection to context";};
+      }
+      {
+        key = "<leader>ar";
+        action = "<cmd>CodeCompanionActions<cr>";
+        mode = ["n"];
+        options = {desc = "actions";};
+      }
+      {
+        key = "<leader>at";
+        action = "<cmd>CodeCompanionChat toggle<cr>";
+        mode = ["n"];
+        options = {desc = "chat";};
+      }
+    ];
+
+    # ai assistant
     plugins.codecompanion = {
       enable = true;
 
       settings = {
         adapters = {
           gemini = {
+            # use 1password to retrieve api key
             __raw = ''
               function()
                 return require('codecompanion.adapters').extend('gemini', {
