3171e2e2ae5a02a807746af3baa8344e79cc9c39
Shanon McQuay <hi@shan.dog>
2025-07-03 07:09:55 +1000

    refactor(kitty): use action alias

--- nix/homeModules/gui/kitty/default.nix
+++ nix/homeModules/gui/kitty/default.nix
@@ -4,19 +4,23 @@
     kitty = {
       enable = true;
 
+      actionAliases = {
+        "focus_tab" = "launch --type=overlay kitty @ focus-tab";
+      };
+
+      keybindings = {
+        "alt+e" = "focus_tab --match title:^editor$";
+        "alt+g" = "focus_tab --match title:^git$";
+        "alt+l" = "focus_tab --match title:^logs$";
+        "alt+p" = "focus_tab --match title:^processes$";
+        "alt+s" = "focus_tab --match title:^shell$";
+      };
+
       settings = {
         # allow controlling kitty from scripts
         allow_remote_control = "yes";
         # allow using the alt key
         macos_option_as_alt = "both";
-      };
-
-      keybindings = {
-        "alt+e" = "launch --type=overlay kitty @ focus-tab --match title:^editor$";
-        "alt+g" = "launch --type=overlay kitty @ focus-tab --match title:^git$";
-        "alt+l" = "launch --type=overlay kitty @ focus-tab --match title:^logs$";
-        "alt+p" = "launch --type=overlay kitty @ focus-tab --match title:^processes$";
-        "alt+s" = "launch --type=overlay kitty @ focus-tab --match title:^shell$";
       };
     };
   };
